f6ca620fd98b79f24f2873b8ac160079face459e
Niklas Zeiler <zeiler.niklas@outlook.de>
2025-05-07 08:10:27 +0200

    fix pwa

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -22,6 +22,10 @@ export const appConfig: ApplicationConfig = {
     provideServiceWorker('ngsw-worker.js', {
         enabled: !isDevMode(),
         registrationStrategy: 'registerWhenStable:30000'
+    }),
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
     })
 ],
 };
--- src/main.ts
+++ src/main.ts
@@ -6,3 +6,8 @@ import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('ngsw-worker.js');
+}
+
